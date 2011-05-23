a0b3b75da95ad2b72dbef0c7483a352d98edeed4
Pavel Zhukov <zeus@MacBook-Pro-Pavel-Zhukov.local>
2011-05-23 16:46:32 +0400

    Allow setting categories in product admin page

--- shop_simplecategories/admin.py
+++ shop_simplecategories/admin.py
@@ -1,5 +1,36 @@
 #-*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from shop_simplecategories.models import Category
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+class ProductWithCategoryForm(forms.ModelForm):
+  categories = forms.ModelMultipleChoiceField(
+    queryset=Category.objects.all(),
+    required=False,
+    widget=FilteredSelectMultiple(
+      verbose_name=_('categories'),
+      is_stacked=False
+    )
+  )
+  def __init__(self, *args, **kwargs):
+    super(ProductWithCategoryForm, self).__init__(*args, **kwargs)
+
+    if self.instance and self.instance.pk:
+      self.fields['categories'].initial = self.instance.categories.all()
+
+  def save(self, commit=True):
+    product = super(ProductWithCategoryForm, self).save(commit=False)
+
+    if commit:
+      product.save()
+
+    if product.pk:
+      product.categories = self.cleaned_data['categories']
+      self.save_m2m()
+
+    return product
+
 
 admin.site.register(Category)
